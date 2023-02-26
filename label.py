782bfae0a95ca3964dd330169878f8abd6c0009f
IkuzO <Kuznetsovilya469@yandex.ru>
2023-02-26 23:41:41 +0300

    menu

--- label.py
+++ label.py
@@ -1,7 +1,8 @@
 class Label:
-    def __init__(self, title, text, idTemp):
+    def __init__(self, title, text, date, idTemp):
         self.id = idTemp
         self.title = title
+        self.date = date
         self.text = text
 
     def info(self):
---
+++ menu.py
@@ -0,0 +1,41 @@
+
+def menu_item(arg):
+    while arg.isdigit() != True:
+        print('\n Не верный ввод')
+        arg = input('Выберите пункт меню: ')
+    return int(arg)
+
+
+def menu():
+    notes = []
+    f = False
+    while f == False:
+        print("Выберите пунк меню:\n\
+              1-Просмотр заметок\n\
+              2-Импортировать заметки\n\
+              3-Экспортировать заметки\n\
+              4-Добавить заметку\n\
+              5-Редактировать заметку\n\
+              6-Удалить заметку\n\
+              0-Выход")
+        choice = menu_item(input('Введите значение: '))
+        if choice == 1:
+            print("просмотр")
+        elif choice == 2:
+            print("Импорт")
+        elif choice == 3:
+            print("Экспорт")
+        elif choice == 4:
+            print("Добавление")
+        elif choice == 5:
+            print("Редактирование")
+        elif choice == 6:
+            print("Удаление")
+        elif choice == 0:
+            print("Программа завереша")
+            f = True
+        else:
+            print("Выбран несуществующий пункт!")
+
+
+menu()
